3d934f02b4d744d472bfafaa6ea4aa2d894aacf5
dang_thi_thu_ha <hadttth1808050@fpt.edu.vn>
2019-04-22 21:04:58 +0700

    baitap3

--- src/main/mainTamGiac.java
+++ src/main/mainTamGiac.java
@@ -4,7 +4,7 @@ public class mainTamGiac {
     public static void main(String[] args){
         TamGiac tamgiac = new TamGiac();
         tamgiac.input();
-        System.out.println("chu vi tam giac la "+tamgiac.chuvi());
-        System.out.printf("dien tich tam giac la "+tamgiac.dientich());
+        System.out.println("chu vi tam giac la: "+tamgiac.chuvi());
+        System.out.printf("dien tich tam giac la: "+tamgiac.dientich());
     }
 }
